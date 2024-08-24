70a8020171c93b0fd48f4396d5e1fefeb908b198
bibek <bamatya1@gmail.com>
2024-08-24 11:21:52 +0545

    fixed darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
 import "./index.css";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Details from "./components/details.tsx";
 import Index from "./index.tsx";
-import PageNotFound from "./components/pageNotFound.tsx";
-import Test from "./components/test.tsx";
+import useDarkMode from "./hooks/useDarkmode.ts";
+import { useEffect } from "react";
 
-export const App = () => (
-  <BrowserRouter>
-    <div className="min-h-screen bg-white dark:bg-gradient-to-b dark:from-gray-900 dark:to-gray-800">
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/projects/:name" element={<Details />} />
-        <Route path="/*" element={<PageNotFound />} />
-        <Route path="/test" element={<Test />} />
-      </Routes>
+export const App = () => {
+  const [isDarkMode] = useDarkMode();
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
+  return (
+    <div className="min-h-screen dark:bg-gradient-to-b dark:from-gray-900 dark:to-gray-800">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/projects/:name" element={<Details />} />
+          <Route path="/*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  </BrowserRouter>
-);
+  );
+};
